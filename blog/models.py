a5582d2841d15dbd8e9141b47b38cfdca5df54b2
Tiago Piperno Bonetti <tiagopiperno@gmail.com>
2022-08-23 22:19:50 -0300

    Adicionado Ckeditor e admin-interface

--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,13 @@ from tabnanny import verbose
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 
 class Post(models.Model):
     titulo = models.CharField(max_length=300)
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
-    conteudo = models.TextField()
+    #conteudo = models.TextField()
+    conteudo = RichTextField()
     data = models.DateTimeField(default=timezone.now)
 
     def __str__(self) -> str:
