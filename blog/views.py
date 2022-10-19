6bdff57b7e7ec9c7ab6dbf43f87cb56d8c74e72b
Tiago Piperno Bonetti <tiagopiperno@gmail.com>
2022-10-18 21:46:21 -0300

    Template atualizado

--- blog/views.py
+++ blog/views.py
@@ -3,15 +3,12 @@ from django.shortcuts import render
 from .models import Post
 
 def post_list(request):
-
     posts = Post.objects.all()
-
-    return render(request, 'blog/home.html', {'posts' : posts })
+    return render(request, 'blog/home.html', {'posts' : posts})
 
 def post_detalhe(request, post_id):
-
     post = Post.objects.get(pk=post_id)
+    return render(request, 'blog/post_detalhe.html', {'post' : post})
 
-    return render(request, 'blog/post_detalhe.html', {'post' : post })
 
 
