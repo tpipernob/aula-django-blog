f8bbc5e49051e1470777c82c9d5e75b853184004
Tiago Piperno Bonetti <tiagopiperno@gmail.com>
2022-10-18 22:12:31 -0300

    Criação das views genericas

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,27 @@
 from urllib import request
 from django.shortcuts import render
 from .models import Post
+from django.views import generic
 
-def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/home.html', {'posts' : posts})
+class post_list(generic.ListView):
+    model = Post
+    template_name = 'blog/home.html'
 
-def post_detalhe(request, post_id):
-    post = Post.objects.get(pk=post_id)
-    return render(request, 'blog/post_detalhe.html', {'post' : post})
+    def get_queryset(self):
+        return Post.objects.all().order_by('-data')[:3]
+
+
+# def post_list(request):
+#     posts = Post.objects.all()
+#     return render(request, 'blog/home.html', {'posts' : posts})
+
+class post_detalhe(generic.DetailView):
+    model = Post
+    template_name = 'blog/post_detalhe.html'
+
+# def post_detalhe(request, post_id):
+#     post = Post.objects.get(pk=post_id)
+#     return render(request, 'blog/post_detalhe.html', {'post' : post})
 
 
 
