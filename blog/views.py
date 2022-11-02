2844a78fd59390f20c33208efe3e68ed83998269
Tiago Piperno Bonetti <tiagopiperno@gmail.com>
2022-11-01 22:15:11 -0300

    Crud via interface

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from .models import Post
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 class post_list(generic.ListView):
     model = Post
@@ -23,5 +24,17 @@ class post_detalhe(LoginRequiredMixin, generic.DetailView):
 #     post = Post.objects.get(pk=post_id)
 #     return render(request, 'blog/post_detalhe.html', {'post' : post})
 
+class post_new(LoginRequiredMixin, generic.CreateView):
+    model = Post
+    template_name = 'blog/post_new.html'
+    fields = "__all__"
 
+class post_edit(LoginRequiredMixin, generic.UpdateView):
+    model = Post
+    template_name = 'blog/post_edit.html'
+    fields = ('titulo', 'conteudo')
 
+class post_delete(LoginRequiredMixin, generic.DeleteView):
+    model = Post
+    template_name = 'blog/post_delete.html'
+    success_url = reverse_lazy('home')
